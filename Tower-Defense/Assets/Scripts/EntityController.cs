70c5dce0518d4bc939bbfadc8741c79c619ca259
sourav-paul <srvpul@gmail.com>
2017-08-26 00:54:13 +0600

    Rts projects ongoing :)

---
+++ Tower-Defense/Assets/Scripts/EntityController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum Entities
+{
+    DEFENCIVE,
+    ATTAKING,
+    HYBRIDE
+}
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class EntityController : MonoBehaviour
+{
+    [SerializeField]
+    private float hitpoint = 100f;
+
+    [SerializeField]
+    private int manaCost = 3;
+
+    [SerializeField]
+    private float attackDistance = 1f;
+
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform target;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
